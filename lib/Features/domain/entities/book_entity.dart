d365714097ce081a03c76d40ba454e06bcef2101
Your Name <you@example.com>
2023-09-12 01:33:53 +0300

    first commit

---
+++ lib/Features/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntitiy {
+  final String image;
+  final String title;
+  final String authorName;
+  final num price;
+  final num rating;
+
+  BookEntitiy(this.image, this.title, this.authorName, this.price, this.rating);
+}
