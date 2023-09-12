430bbd7eb5583ecb462525c81af637a1975d9359
Your Name <you@example.com>
2023-09-12 03:09:55 +0300

    refactor to use_case class

--- lib/Features/domain/usecase/fetch_feature_book_use_case.dart
+++ lib/Features/domain/usecase/fetch_feature_book_use_case.dart
@@ -1,13 +1,18 @@
 import 'package:bookly/Features/domain/entities/book_entity.dart';
 import 'package:bookly/Features/domain/repos/home_repo.dart';
 import 'package:bookly/core/error/failure.dart';
+import 'package:bookly/core/use_case/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeatureBookUseCase {
+class FetchFeatureBookUseCase extends UseCase<List<BookEntitiy>,void> {
   final HomeRepo homeRepo;
-  FetchFeatureBookUseCase(this.homeRepo);
-    Future<Either<Failure, List<BookEntitiy>>> fetchFeatureBooks(){
-      return homeRepo.fetchFeatureBooks();
-    }
 
+  FetchFeatureBookUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<BookEntitiy>>> call([void param]) {
+    return homeRepo.fetchFeatureBooks();
+  }
 }
+
+
