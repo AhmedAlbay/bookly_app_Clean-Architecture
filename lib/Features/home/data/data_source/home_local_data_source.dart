8ce01dfc6802992eeb83252e06204e6eb4541fd9
Your Name <you@example.com>
2023-09-13 02:53:49 +0300

    update

--- lib/Features/home/data/data_source/home_local_data_source.dart
+++ lib/Features/home/data/data_source/home_local_data_source.dart
@@ -8,11 +8,13 @@ List<BookEntitiy> fetchNewestBooks();
 class HomeLocalDataSourceImpl extends HomeLocalDataSource {
   @override
   List<BookEntitiy> fetchFeatureBooks() {
-
+    // TODO: implement fetchFeatureBooks
+    throw UnimplementedError();
   }
 
   @override
   List<BookEntitiy> fetchNewestBooks() {
-
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
   }
 }
